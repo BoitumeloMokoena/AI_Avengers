72c175627e58a598295a9556e5316a03586eb07e
tncube023a <tncube023a@student.wethinkcode.co.za>
2024-04-27 14:29:51 +0200

    edited some stuff

--- Menu.py
+++ Menu.py
@@ -3,14 +3,25 @@ def on_display():
     print("Menu\n")
     print("1. Tax Estimation\n2.Exit")
 
+    option = input(int("Enter an option between 1 and 2"))
+
+    while(option == 1):
+        tax_estimation()
+    else:
+        print("Exiting program...")
+        
+
 def get_companyName():
     companyName=input(str("Input your company name: "))
 
 
-def assests():
+def get_country():
+    country=input(str("Please enter your country: "))
+
+def get_assests():
     assestsValue=input(float("Input the toatal depreciation and armotization value of your assests: "))
     
-def income():
+def get_income():
 
     income=input(float("Please enter your income: ")) 
 
@@ -19,16 +30,22 @@ def income():
         break
         
 
-def expenses():
+def get_expenses():
     expense=input(float("Please enter your total expenses: "))
 
 
 def calculate_taxable_income(income, assets, expenses):
     gross_income = income - (assets + expenses)
-    return income
+    return income*0.28
 
-def get_business_info():
-    on_display()
+def tax_estimation():
+    get_companyName()
+    get_country()
+    get_assests()
+    get_income()
+    get_expenses()
+    calculate_taxable_income()
+
 
 
 
